2ff1fa627e048d123e23ba376b4daf2213c3a091
jasondansie <jasondansie@gmail.com>
2023-02-28 14:59:01 +0200

    adds status cods file

--- Storage/createDatabase.js
+++ Storage/createDatabase.js
@@ -12,9 +12,9 @@ let createStatementFile='./createStatements.json';
 let adminPass = '';
 
 if(process.argv.length>2){
-    adminPass=process.argv[2];
+    adminPass = process.argv[2];
     if(process.argv.length>3){
-        createStatementFile=`./${process.argv[3]}`;
+        createStatementFile =`./${process.argv[3]}`;
     }
 }
 
@@ -89,11 +89,8 @@ async function createDb(createStatements, adminPass){
                 if(DEBUG) printMessage('Table columns missing. Table was not created.');
             }
         }
-
-
     }
     catch(error){
         printError(error);
     }
-
 }
--- Storage/database.js
+++ Storage/database.js
@@ -40,6 +40,4 @@ module.exports=class Database{
             }
         });
     }
-
-
 }
